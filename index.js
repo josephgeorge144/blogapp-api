be9c15ea9339dd92306a10dbc7eb4f5c9f28ee83
Joseph George Venmelil <84468450+josephgeorge144@users.noreply.github.com>
2022-12-16 13:50:22 +0000

    2nd commit

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+
 
 dotenv.config()
-function calculateDaysBetweenDates(begin, end){}
+app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,useUnifiedTopology:true },(err)=>{
     if(err)console.log(err);
@@ -15,8 +17,9 @@ app.listen("5000", () => {
     console.log("Backend is running.");
   });
 
-app.use('/',(req,res)=>{
-    console.log('this is q url');
-    res.status(200).json('asr')
-})
+// app.use('/',(req,res)=>{
+//     console.log('this is q url');
+//     res.status(200).json('asr')
+// })
+app.use("/api/auth", authRoute);
 
